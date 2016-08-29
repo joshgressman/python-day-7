034f7d8554e30dcc425c9a57884a1c37b4ef1ee5
joshgressman <joshgressman@gmail.com>
2016-08-29 12:28:33 -0500

    python day seven

---
+++ python_day7.js
@@ -0,0 +1,32 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 26 2016, 10:47:25)
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+
+>>> equal_to_five = 5 == 5
+>>> equal_to_five
+True
+>>> #equal to five will return true
+>>> 5 < 3
+False
+>>> 5 == 5
+True
+>>> numbers = [1,2,3,4,5,6,7,8,9,10]
+>>> numbers
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+>>> for number in numbers:
+	print(number > 5)
+
+
+False
+False
+False
+False
+False
+True
+True
+True
+True
+True
+>>> #iterates through array, evaluates if number is >5 returns boolien
